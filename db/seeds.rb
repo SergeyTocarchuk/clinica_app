37cf0212334b9f3443d7bfc5bfe57a99d9c9a101
Serhii T <tocarchuk@gmail.com>
2022-05-05 16:15:57 +0200

    comment ffaker in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,36 +16,36 @@ categories.each do |category|
   Category.create(title: "#{category}")
 end
 
-1..20.times do |num|
-  Doctor.create(email: "doctor_#{num}@clinic.com", 
-                password: 'password', 
-                password_confirmation: 'password', 
-                name: FFaker::Name.name,
-                category_id: 1)
-end
+# 1..20.times do |num|
+#   Doctor.create(email: "doctor_#{num}@clinic.com", 
+#                 password: 'password', 
+#                 password_confirmation: 'password', 
+#                 name: FFaker::Name.name,
+#                 category_id: 1)
+# end
 
-21..40.times do |num|
-  Doctor.create(email: "doctor_#{num}@clinic.com", 
-                password: 'password', 
-                password_confirmation: 'password', 
-                name: FFaker::Name.name,
-                category_id: 2)
-end
+# 21..40.times do |num|
+#   Doctor.create(email: "doctor_#{num}@clinic.com", 
+#                 password: 'password', 
+#                 password_confirmation: 'password', 
+#                 name: FFaker::Name.name,
+#                 category_id: 2)
+# end
 
-41..60.times do |num|
-  Doctor.create(email: "doctor_#{num}@clinic.com", 
-                password: 'password', 
-                password_confirmation: 'password', 
-                name: FFaker::Name.name,
-                category_id: 3)
-end
+# 41..60.times do |num|
+#   Doctor.create(email: "doctor_#{num}@clinic.com", 
+#                 password: 'password', 
+#                 password_confirmation: 'password', 
+#                 name: FFaker::Name.name,
+#                 category_id: 3)
+# end
 
-61..80.times do |num|
-  Doctor.create(email: "doctor_#{num}@clinic.com", 
-                password: 'password', 
-                password_confirmation: 'password', 
-                name: FFaker::Name.name,
-                category_id: 4)
-end
+# 61..80.times do |num|
+#   Doctor.create(email: "doctor_#{num}@clinic.com", 
+#                 password: 'password', 
+#                 password_confirmation: 'password', 
+#                 name: FFaker::Name.name,
+#                 category_id: 4)
+# end
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
