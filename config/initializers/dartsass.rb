15c67f3e343de6e47e3cc5cf9a3d12b055f312d7
Serhii T <tocarchuk@gmail.com>
2022-05-09 17:09:51 +0200

    change for precompile

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,7 +1,7 @@
 # The hash key is the relative path to a Sass file in app/assets/stylesheets/ 
 # and the hash value will be the name of the file output to app/assets/builds/
 
-# Rails.application.config.dartsass.builds = {  
-#   "application.scss"        => "application.css",
-#   "active_admin.scss"       => "active_admin.css"
-# }
+Rails.application.config.dartsass.builds = {  
+  "application.scss"        => "application.css",
+  "active_admin.scss"       => "active_admin.css"
+}
